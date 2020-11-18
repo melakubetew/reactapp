71b943c1379a25a334383213bd9fe55bd61eb16d
melakubetew <68308699+melakubetew@users.noreply.github.com>
2020-11-18 11:58:08 -0500

    Create Footer.js

---
+++ Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Footer extends React.Component{
+    render(){
+        return (
+          <div style={{color: "#FF3342"}}> This is Footer </div>
+        )
+      }
+      
+        
+      }
+      export default Footer;
+      
