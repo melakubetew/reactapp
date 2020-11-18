952cc94394359f85fd68047d7030f4aafbe45c1c
melakubetew <68308699+melakubetew@users.noreply.github.com>
2020-11-18 11:50:40 -0500

    Create Header.js

---
+++ Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+class Header extends React.Component{
+    render(){
+        return (
+          <div style={{color: "4CFF33"}}> This is Header </div>
+        )
+      }
+      
+        
+      }
+      export default Header;
