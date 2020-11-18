cde6f8dbd118c60c4cb1ac8437740a1057d3443e
melakubetew <68308699+melakubetew@users.noreply.github.com>
2020-11-18 11:56:42 -0500

    Create Body.js

---
+++ Body.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Body extends React.Component{
+    render(){
+        return (
+          <div style={{color: "FFE933"}}> This is Body </div>
+        )
+      }
+      
+        
+      }
+      export default Body;
+      
